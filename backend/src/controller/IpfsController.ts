d1d19cf99b036eea5a745fbbf166b5c428f1bd2d
Toumi-Elyes <elyes.toumi@epitech.eu>
2022-05-07 19:40:04 +0200

    add: call to starton api to upload files

--- backend/src/controller/IpfsController.ts
+++ backend/src/controller/IpfsController.ts
@@ -1,12 +1,59 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import FormData from 'form-data';
+
+dotenv.config();
+
+const { STARTON_API_KEY } = process.env;
+const starton: any = axios.create(
+  {
+    baseURL: "https://api.starton.io/v2",
+    headers: {
+        "x-api-key": `${STARTON_API_KEY}`,
+    },
+  }
+)
+
+interface File {
+    name: string,
+    file: any
+}
 
 export default class IpfsController {
     async uploadData(req: express.Request, res: express.Response) : Promise<express.Response | null>
     {
-        return res.status(200).send("Upload data from ipfs.");
+        let data = new FormData();
+        let files: any = req.files;
+
+        console.log(files[0]);
+        const params : File = {
+            name: (files[0].originalname != undefined) ? files[0].originalname : "NoName",
+            file: files[0].buffer
+        };
+
+        data.append("file", params.file, params.name);
+        data.append("isSync", "true");
+
+        const ipfsImg = await starton.post("/pinning/content/file", data, {
+            maxBodyLength: "Infinity",
+            headers: { "Content-Type": `multipart/form-data; boundary=${data.getBoundary()}` },
+        }).catch((error : any) => { console.log(error) });
+
+        return res.status(200).send(ipfsImg.data);
     }
+
     async getData(req: express.Request, res: express.Response) : Promise<express.Response | null>
     {
-        return res.status(200).send("get data from ipfs.");
+        const id : string = req.body.request_id;
+
+        const ipfsImg = await starton.get("/pinning/content/".concat(id), {
+            maxBodyLength: "Infinity",
+            headers: { "Content-Type": `multipart/form-data` },
+        }).catch((error : any) => { console.log(error) });
+
+        const ipfsFile = await axios.get("https://ipfs.io/ipfs/".concat(ipfsImg.data.pinStatus.pin.cid));
+
+        return res.status(200).send(ipfsFile.data);
     }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import auth from './middleware/auth';
 import IpfsRouter from './router/ipfsRouter'
+import bodyParser from 'body-parser';
+import multer from 'multer';
+
 const port = 3000;
 const app = express();
+let upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array('file'));
+app.use(express.static('public'));
 
 app.use(express.json({limit: '2mb'}))
-app.use("/ipfs/",auth, IpfsRouter)
+app.use("/ipfs/", IpfsRouter)
 app.listen(port, () => console.log(`Ready on port ${port}`))
