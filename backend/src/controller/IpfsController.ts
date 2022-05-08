914192849bb359f550afbaa5640e631006eaf299
Toumi-Elyes <elyes.toumi@epitech.eu>
2022-05-08 03:14:42 +0200

    add: docker-compose for backend

--- backend/src/controller/IpfsController.ts
+++ backend/src/controller/IpfsController.ts
@@ -17,43 +17,67 @@ const starton: any = axios.create(
 
 interface File {
     name: string,
-    file: any
+    file: string
 }
 
 export default class IpfsController {
     async uploadData(req: express.Request, res: express.Response) : Promise<express.Response | null>
     {
-        let data = new FormData();
-        let files: any = req.files;
+        try {
+            let data = new FormData();
+            let files: any = req.files;
 
-        console.log(files[0]);
-        const params : File = {
-            name: (files[0].originalname != undefined) ? files[0].originalname : "NoName",
-            file: files[0].buffer
-        };
+            if (files == null) {
+                return res.status(400).json({ message: 'No files were uploaded.' });
+            }
+            console.log(typeof(files[0]), files[0].buffer.toString().length);
+            const params : File = {
+                name: (files[0].originalname != undefined) ? files[0].originalname : "NoName",
+                file: files[0].buffer.toString()
+            };
 
-        data.append("file", params.file, params.name);
-        data.append("isSync", "true");
+            data.append("file", params.file, params.name);
+            data.append("isSync", "true");
 
-        const ipfsImg = await starton.post("/pinning/content/file", data, {
-            maxBodyLength: "Infinity",
-            headers: { "Content-Type": `multipart/form-data; boundary=${data.getBoundary()}` },
-        }).catch((error : any) => { console.log(error) });
+            try {
+                const ipfsImg = await starton.post("/pinning/content/file", data, {
+                    maxBodyLength: "Infinity",
+                    headers: { "Content-Type": `multipart/form-data; boundary=${data.getBoundary()}`},
+                });
+                return res.status(200).send(ipfsImg.data);
+            } catch (error) {
+                console.error(error);
+                return res.status(500).send('Error while sending data to starton server.');
+            }
 
-        return res.status(200).send(ipfsImg.data);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Error while sending data to starton server');
+        }
     }
 
     async getData(req: express.Request, res: express.Response) : Promise<express.Response | null>
     {
         const id : string = req.body.request_id;
 
-        const ipfsImg = await starton.get("/pinning/content/".concat(id), {
-            maxBodyLength: "Infinity",
-            headers: { "Content-Type": `multipart/form-data` },
-        }).catch((error : any) => { console.log(error) });
+        if (id == null) {
+            return res.status(400).json({ message: 'No request id was provided.' });
+        }
+        try {
+            const ipfsImg = await starton.get("/pinning/content/".concat(id), {
+                maxBodyLength: "Infinity",
+                headers: { "Content-Type": `multipart/form-data` },
+            })
+            .catch(() => {
+                return res.status(500).send('Error while sending data to starton server');
+            });
 
-        const ipfsFile = await axios.get("https://ipfs.io/ipfs/".concat(ipfsImg.data.pinStatus.pin.cid));
+            const ipfsFile = await axios.get("https://ipfs.io/ipfs/".concat(ipfsImg.data.pinStatus.pin.cid));
 
-        return res.status(200).send(ipfsFile.data);
+            return res.status(200).send(ipfsFile.data);
+
+        } catch (error) {
+            return res.status(500).send('Error while trying to get data from ipfs');
+        }
     }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import auth from './middleware/auth';
+import dotenv from 'dotenv';
 import IpfsRouter from './router/ipfsRouter'
 import bodyParser from 'body-parser';
 import multer from 'multer';
+import cors from 'cors';
 
-const port = 3000;
+const { BACKEND_PORT } = process.env;
 const app = express();
 let upload = multer();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array('file'));
@@ -15,4 +18,4 @@ app.use(express.static('public'));
 
 app.use(express.json({limit: '2mb'}))
 app.use("/ipfs/", IpfsRouter)
-app.listen(port, () => console.log(`Ready on port ${port}`))
+app.listen(BACKEND_PORT, () => console.log(`Ready on port ${BACKEND_PORT}`));
